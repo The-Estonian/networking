a7f63b5643c0359c8bcf6ae1cec7180afc468782
The-Estonian <Zaar2213@gmail.com>
2024-04-11 15:51:11 +0300

    testing IP changing

--- frontend/src/connections/newPostConnection.js
+++ frontend/src/connections/newPostConnection.js
@@ -1,7 +1,4 @@
-const backendUrl =
-  typeof window === 'undefined'
-    ? process.env.REACT_APP_BACKEND_URL
-    : 'http://localhost:8080';
+const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080';
 
 export const SendNewPost = async (formData) => {
   try {
