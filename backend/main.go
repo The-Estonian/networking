1b4d0eb2759d85a785be36102f25864fff302c41
The-Estonian <Zaar2213@gmail.com>
2024-04-12 14:34:45 +0300

    last update before vacation

--- backend/main.go
+++ backend/main.go
@@ -14,14 +14,14 @@ func main() {
 	sqlite.Create()
 	mux := http.NewServeMux()
 	server := &http.Server{
-		Addr:    ":443",
+		Addr:    ":8080",
 		Handler: midware.CorsMiddleware(mux),
 	}
 	urlHandlers.StartHandlers(mux)
 	fmt.Println("Backend running on port 8080")
-	err := server.ListenAndServeTLS("/etc/letsencrypt/live/3.74.83.15/fullchain.pem", "/etc/letsencrypt/live/3.74.83.15/privkey.pem")
+	// err := server.ListenAndServeTLS("/etc/letsencrypt/live/3.74.83.15/fullchain.pem", "/etc/letsencrypt/live/3.74.83.15/privkey.pem"
+	err := server.ListenAndServe()
 	if err != nil {
 		panic(err)
 	}
 }
-
