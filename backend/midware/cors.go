0cc0264b947e7eb8238d85e7eebdef7860b95d78
The-Estonian <Zaar2213@gmail.com>
2024-04-21 19:32:24 +0300

    adjustemnts to CORS and nginx

--- backend/midware/cors.go
+++ backend/midware/cors.go
@@ -9,7 +9,7 @@ func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(r.Header.Get("Origin"))
 		switch origin := r.Header.Get("Origin"); origin {
-		case "http://localhost:3000", "ws://localhost:3000", "http://3.73.175.179", "ws://3.73.175.179", "http://frontend", "ws://frontend":
+		case "http://localhost:3000", "ws://localhost:3000", "http://3.73.175.179", "ws://3.73.175.179":
 			(w).Header().Set("Access-Control-Allow-Origin", origin)
 		}
 		(w).Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
