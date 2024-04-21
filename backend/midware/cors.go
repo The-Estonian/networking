f13b0c19ae084a8633a5842555fe79f29b3261c4
The-Estonian <Zaar2213@gmail.com>
2024-04-21 22:20:06 +0300

    new AWS EC container

--- backend/midware/cors.go
+++ backend/midware/cors.go
@@ -9,7 +9,14 @@ func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(r.Header.Get("Origin"))
 		switch origin := r.Header.Get("Origin"); origin {
-		case "http://localhost:3000", "ws://localhost:3000", "http://3.73.175.179", "ws://3.73.175.179":
+		case "http://localhost:3000",
+			"ws://localhost:3000",
+			"http://www.devpipe.ee",
+			"http://devpipe.ee",
+			"https://www.devpipe.ee",
+			"https://devpipe.ee",
+			"http://3.76.113.233",
+			"ws://3.76.113.233":
 			(w).Header().Set("Access-Control-Allow-Origin", origin)
 		}
 		(w).Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
