9922f0938edfd44dda03312b5130e3a86c9b7160
The-Estonian <Zaar2213@gmail.com>
2024-04-21 15:24:52 +0300

    updated nginx and AWS IP:Ports

--- backend/midware/cors.go
+++ backend/midware/cors.go
@@ -9,10 +9,9 @@ func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(r.Header.Get("Origin"))
 		switch origin := r.Header.Get("Origin"); origin {
-		case "http://localhost:3000", "ws://localhost:3000", "http://frontend", "ws://frontend", "http://3.73.175.179/":
+		case "http://localhost:3000", "ws://localhost:3000", "http://frontend", "ws://frontend":
 			(w).Header().Set("Access-Control-Allow-Origin", origin)
 		}
-		// (w).Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		(w).Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		(w).Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		(w).Header().Set("Access-Control-Allow-Credentials", "true")
