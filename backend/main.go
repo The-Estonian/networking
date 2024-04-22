2291b6c9ee5e861e7359e020c9caa3fadeb5f675
The-Estonian <Zaar2213@gmail.com>
2024-04-22 09:42:52 +0300

    some more adjustments to cloud

--- backend/main.go
+++ backend/main.go
@@ -14,14 +14,14 @@ func main() {
 	sqlite.Create()
 	mux := http.NewServeMux()
 	server := &http.Server{
-		// Addr:    ":8080",
-		Addr:    ":443",
+		Addr: ":8080",
+		// Addr:    ":443",
 		Handler: midware.CorsMiddleware(mux),
 	}
 	urlHandlers.StartHandlers(mux)
 	fmt.Println("Backend running on port 8080")
-	err := server.ListenAndServeTLS("/certs/fullchain.pem", "/certs/privkey.pem")
-	// err := server.ListenAndServe()
+	// err := server.ListenAndServeTLS("/certs/fullchain.pem", "/certs/privkey.pem")
+	err := server.ListenAndServe()
 	if err != nil {
 		panic(err)
 	}
