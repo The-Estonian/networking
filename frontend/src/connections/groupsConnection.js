19382c29045e3b21a95e1af7ea6ad84fa164d680
The-Estonian <Zaar2213@gmail.com>
2024-04-24 12:12:14 +0300

    changed groups backend connection URL's

--- frontend/src/connections/groupsConnection.js
+++ frontend/src/connections/groupsConnection.js
@@ -1,8 +1,9 @@
-const backendUrl = import.meta.env.VITE_APP_BACKEND_URL || 'localhost:8080';
+const backendUrl =
+  import.meta.env.VITE_APP_BACKEND_URL || 'http://localhost:8080';
 
 export const GetAllGroups = async (formData) => {
   try {
-    const response = await fetch(`http://${backendUrl}/groups`, {
+    const response = await fetch(`${backendUrl}/groups`, {
       method: 'POST',
       mode: 'cors',
       cache: 'no-cache',
@@ -11,10 +12,10 @@ export const GetAllGroups = async (formData) => {
       body: formData,
       credentials: 'include',
     });
-    const resp = await response.json()
-    return resp
+    const resp = await response.json();
+    return resp;
   } catch (error) {
-    console.log('Groups error')
-    console.log(error)
+    console.log('Groups error');
+    console.log(error);
   }
-}
+};
