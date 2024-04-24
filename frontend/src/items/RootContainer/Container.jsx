8cbb1b5676bcb5302c2fd8c1bac09c17ec5080ee
The-Estonian <Zaar2213@gmail.com>
2024-04-24 10:30:30 +0300

    adjusted websocket url

--- frontend/src/items/RootContainer/Container.jsx
+++ frontend/src/items/RootContainer/Container.jsx
@@ -7,9 +7,9 @@ import useWebSocket from 'react-use-websocket';
 const backendUrl =
   import.meta.env.VITE_APP_BACKEND_PICTURE_URL || 'localhost:8080';
 let websock = `ws://${backendUrl}/websocket`;
-// if (backendUrl != 'localhost:8080') {
-//   websock = `wss://${backendUrl.substring(8)}/websocket`;
-// }
+if (backendUrl != 'localhost:8080') {
+  websock = `ws://${backendUrl.substring(8)}/websocket`;
+}
 
 import Menu from '../Menu/Menu';
 import Authenticate from '../../authentication/Authenticate.jsx';
